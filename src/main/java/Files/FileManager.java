85730a4a89562637bcfa26fc895743b3dfa83f15
Dragothief <evangarcia48@gmail.com>
2023-04-14 14:25:24 -0400

    H

--- src/main/java/Files/FileManager.java
+++ src/main/java/Files/FileManager.java
@@ -1,26 +1,50 @@
 package Files;
 
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Map;
+
 
 import Authentication.User;
 
 public class FileManager {
 
-    public static void writeUserToFile(User user, String fileName) {
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
-            oos.writeObject(user);
+    public static void writeCredentialsToFile(Map<String, User> userMap) {
+        String filename = "credentials.txt";
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("credentials.txt", true))) {
+            for (User user : userMap.values()) {
+                bw.write("Username: " + user.getUsername() + "\n");
+                bw.write("Password: " + user.getPassword().toString() + "\n");
+                bw.write("Email: " + user.getEmail() + "\n\n");
+            }
+            System.out.println("Credentials sent to file");
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Error writing credentials to file: " + e.getMessage());
         }
     }
-
-    public static User readUserFromFile(String fileName) {
-        User user = null;
-        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName))) {
-            user = (User) ois.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
+    
+    public static void searchCredentialsByUsername(String username, String filename) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            boolean found = false;
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("Username: ")) {
+                    String currentUsername = line.substring("Username: ".length()).trim();
+                    if (currentUsername.equals(username)) {
+                        found = true;
+                        System.out.println("Credentials found for username " + username + ":");
+                        System.out.println("Password: " + br.readLine().substring("Password: ".length()));
+                        System.out.println("Email: " + br.readLine().substring("Email: ".length()) + "\n");
+                        break;
+                    }
+                }
+            }
+            if (!found) {
+                System.out.println("Credentials not found for username " + username);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading credentials from file: " + e.getMessage());
         }
-        return user;
     }
 }
